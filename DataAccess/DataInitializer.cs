e15e8ea0bb424e796adb62131fc4aa48403ab043
Columbia91 <alpha.z91@mail.ru>
2019-05-08 21:39:52 +0600

    Добавьте файлы проекта.

---
+++ DataAccess/DataInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using Services;
+
+namespace DataAccess
+{
+    public class DataInitializer : CreateDatabaseIfNotExists<SecurityContext>
+    {
+        protected override void Seed(SecurityContext context)
+        {
+            context.Users.Add(new Models.User
+            {
+                Login = "admin",
+                Password = DataEncryptor.HashPassword("123")
+            });
+        }
+    }
+}
